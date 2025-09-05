4b3d478817bea74ba8346b5932082405d7c6d99f
koza <djkoza@gmail.com>
2025-09-05 11:49:53 +0200

    load yaml configuration file from given environment variable path

--- mqtt_gpio/config.py
+++ mqtt_gpio/config.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from typing import Any, Dict
+import os
 import yaml
 
-def load_config(path: str = "config.yaml") -> Dict[str, Any]:
-    """Load YAML configuration file."""
+def load_config(default_path: str = "config.yaml") -> Dict[str, Any]:
+    """
+    Load YAML configuration file.
+    Priority:
+      1. Environment variable MQTT_GPIO_CONFIG
+      2. Provided default_path
+    """
+    path = os.environ.get("MQTT_GPIO_CONFIG", default_path)
     with open(path, "r") as f:
         return yaml.safe_load(f)
